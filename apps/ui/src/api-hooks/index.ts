8ba0b75be7fbcdcd535c11db6cb2371af4c36962
Luis Barrios <cl.bs.xv@gmail.com>
2024-04-05 19:32:02 -0400

    added header for ignoring csp

--- apps/ui/src/api-hooks/index.ts
+++ apps/ui/src/api-hooks/index.ts
@@ -6,6 +6,9 @@ const baseURL = import.meta.env.VITE_SERVER_URL;
 const cfg = { baseURL };
 
 export const ctx = axios.create(cfg);
+ctx.defaults.headers.common[
+  "Content-Security-Policy"
+] = `default-src 'self' ${baseURL};`;
 
 export const fetchRooms = async (): Promise<Todo> => {
   const { rooms } = (await ctx.get("/rooms/get")).data;
